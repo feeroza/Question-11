e0ca8ab0c020a77934c9e497dd725f9c7f3edd89
feeroza <160626229+feeroza@users.noreply.github.com>
2024-08-08 22:02:58 +0500

    Add files via upload

---
+++ Question 11/011.js
@@ -0,0 +1,6 @@
+// 11. Names: Store the names of a few of your friends in a array called names.
+// Print each person’s name by accessing each element in the list, one at a time.
+var myFriends = ["bisma", "fatima", "eisha"];
+console.log(myFriends[0]);
+console.log(myFriends[2]);
+console.log(myFriends[1]);
